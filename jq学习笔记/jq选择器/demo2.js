162ae5648399fab693b1c18b7c0ca7f7827955bf
xiaozonghan <1597138566@qq.com>
2017-03-22 21:53:40 +0800

    3.22

---
+++ jq学习笔记/jq选择器/demo2.js
@@ -0,0 +1,44 @@
+//标签选择器
+// $(function(){
+// 	$("div").css("color","blue");
+// });
+
+// // id选择器
+// $(function(){
+// 	$("#div").css("color","red");
+// });
+
+// 类选择器
+// $(function(){
+// 	$(".box").css("color","yellow");
+// });
+
+// 组选择器
+// $(function(){
+// 	$("div,#div,.box").css("color","#ccc");
+// });
+
+//后代选择器
+// $(function(){
+// 	$("ul li").css("color","green");
+// });
+
+// 两种特殊选择器用法
+// $(function(){
+// 	$(".box.pox").css("color","blue");
+// })
+
+$(function(){
+	$("div.kox").css("color","red");
+});
+
+
+
+
+
+
+
+
+
+
+
